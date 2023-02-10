230b570738a2f21fafe280860e6130bc4160b9a9
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-10 22:38:10 +0200

    details rest fix, restaurant. Clear images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import './JS/about-hotel-slider';
-import './JS/details-rest-slider';
-import './JS/details-hotel-slider';
-import './JS/hotel-slider';
-import './JS/modal-room';
-import './JS/modal-price';
-import './JS/modal';
+import "./JS/details-rest-slider";
+import "./JS/details-hotel-slider";
+import "./JS/box-slider";
+import "./JS/modal-room";
+import "./JS/modal-price";
+import "./JS/modal";
